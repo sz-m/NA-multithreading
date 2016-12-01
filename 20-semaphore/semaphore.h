2ff34c614bf9765bed43971cbc03eed439115874
_michal <sz-m@users.noreply.github.com>
2016-12-01 10:52:38 +0000

    add 20-semaphore implementation

--- 20-semaphore/semaphore.h
+++ 20-semaphore/semaphore.h
@@ -1,13 +1,50 @@
 #pragma once
 
 #include <cstddef>
+#include <condition_variable>
+#include <mutex>
 
 class semaphore
 {
 public:
-    semaphore(std::size_t count);
-    void wait();
-    bool try_wait();
-    void signal(std::size_t count = 1);
+  semaphore(std::size_t count = 0) : _count{count}
+  {
+  }
+  
+  void wait()
+  {
+    std::unique_lock<std::mutex> lock{_mtx};
+    _cv.wait(lock, [&](){ return _count != 0; });
+    --_count;
+  }
+
+  bool try_wait()
+  {
+    std::unique_lock<std::mutex> lock{_mtx};
+
+    if(_count > 0)
+    {
+      _count--;
+      return true;
+    }
+
+    return false;
+  }
+
+  void signal(std::size_t count = 1)
+  {
+    std::unique_lock<std::mutex> lock{_mtx};
+    _count += count;
+    while(count--)
+    {
+      _cv.notify_one();
+    }
+  }
+
+private:
+  std::mutex _mtx;
+  std::condition_variable _cv;
+
+  std::size_t _count;
 };
 
