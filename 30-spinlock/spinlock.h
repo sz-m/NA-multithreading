8f941a68d0c9e31887cc39deb58ed5ad448cf0d4
_michal <sz-m@users.noreply.github.com>
2016-12-01 12:43:18 +0000

    add 30-spinlock implementation

--- 30-spinlock/spinlock.h
+++ 30-spinlock/spinlock.h
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <atomic>
+
 class spinlock
 {
 public:
-    void lock();
-    bool try_lock();
-    void unlock();
+  void lock()
+  {
+    while(!try_lock())
+    {
+     asm volatile ("pause" : : : "memory"); 
+    }
+  }
+  
+  bool try_lock()
+  {
+    return !_locked.exchange(true);
+  }
+
+  void unlock()
+  {
+    assert(_locked);
+    _locked = false;
+  }
+
+private:
+  std::atomic<bool> _locked{ false };
 };
 
